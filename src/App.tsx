3f073561c9c64c1e1b485ecab0600b2eb05ebcfc
Johann Felipe Rojas <johannrip@gmail.com>
2023-10-27 11:30:36 -0500

    refactor: remove title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useTranslation } from 'react-i18next';
-
 import CreateContact from './components/CreateContact';
 import Gateway from './components/Gateway';
 
@@ -8,21 +6,13 @@ import { getView } from './store/selectors';
 
 export default function App() {
   const view = useAppSelector(getView);
-  const { t } = useTranslation();
 
   return (
-    <>
-      <h1 className="fw-bold fs-4 mb-3">
-        {t(view === 'transfer'
-          ? 'title.transfer'
-          : 'title.createContact')}
-      </h1>
-      <div className="row">
-        <div className="offset-lg-2 col-lg-8 offset-xl-3 col-xl-6">
-          {view === 'transfer' && <Gateway />}
-          {view === 'newContact' && <CreateContact />}
-        </div>
+    <div className="row">
+      <div className="offset-lg-2 col-lg-8 offset-xl-3 col-xl-6">
+        {view === 'transfer' && <Gateway />}
+        {view === 'newContact' && <CreateContact />}
       </div>
-    </>
+    </div>
   );
 }
