1948524dcf7ba09fd139dfc810adb8f79bb83c4c
Johann Felipe Rojas <johannrip@gmail.com>
2025-02-21 16:42:54 -0500

    fix: account name param

--- src/services/mappers/accountMapper.ts
+++ src/services/mappers/accountMapper.ts
@@ -5,7 +5,7 @@ import type { DepositAccount } from '../interface';
 export default function accountMapper(apiAccount: ApiAccount): DepositAccount {
   return {
     id: apiAccount.id,
-    name: apiAccount.account_holder_name,
+    name: apiAccount.account_name,
     accountNumber: apiAccount.masked_number,
     type: ApiAccountTypeConfig[apiAccount.group],
     baseType: apiAccount.type.toLowerCase() as AccountBaseType.Deposit,
