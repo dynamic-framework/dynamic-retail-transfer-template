5aadfd7c17a7c9d159157af932b532072acd417d
lnazarit <lnazarit@modyo.com>
2026-03-13 16:11:36 -0300

    feat: update design

--- src/components/AccountListItem.tsx
+++ src/components/AccountListItem.tsx
@@ -1,4 +1,4 @@
-import { DAvatar } from '@dynamic-framework/ui-react';
+import { DIcon } from '@dynamic-framework/ui-react';
 import classNames from 'classnames';
 import { useCallback } from 'react';
 
@@ -19,21 +19,25 @@ export default function AccountListItem({ depositAccount }: Props) {
   }, [depositAccount, dispatch]);
 
   return (
-    <button
-      type="button"
-      onClick={handleSelectContact}
-      className={classNames(
-        'd-flex gap-2 align-items-center justify-content-between w-100 p-2',
-        'border-0 text-start border-bottom border-gray-100 quick-action-button',
-      )}
-    >
-      <div className="d-flex gap-2 align-items-center">
-        <DAvatar name={depositAccount.name} />
+    <div className="border-bottom border-gray-50 py-1">
+      <button
+        type="button"
+        onClick={handleSelectContact}
+        className={classNames(
+          'd-flex gap-2 align-items-center justify-content-between w-100 p-2',
+          'border-0 text-start rounded-2 bg-transparent hover:bg-gray-50 quick-action-button',
+        )}
+      >
         <div>
           <p className="mb-0 fw-bold">{depositAccount.name}</p>
           <small className="text-gray-500">{depositAccount.accountNumber}</small>
         </div>
-      </div>
-    </button>
+        <DIcon
+          className="ms-auto"
+          color="primary"
+          icon="ChevronRight"
+        />
+      </button>
+    </div>
   );
 }
--- src/components/ContactListItem.tsx
+++ src/components/ContactListItem.tsx
@@ -1,4 +1,4 @@
-import { DAvatar, DIcon } from '@dynamic-framework/ui-react';
+import { DIcon } from '@dynamic-framework/ui-react';
 import classNames from 'classnames';
 import { useCallback } from 'react';
 
@@ -19,25 +19,33 @@ export default function ContactListItem({ contact }: Props) {
   }, [contact, dispatch]);
 
   return (
-    <button
-      type="button"
-      onClick={handleSelectContact}
-      className={classNames(
-        'd-flex gap-2 align-items-center justify-content-between w-100 p-2',
-        'border-0 text-start border-bottom border-gray-100 quick-action-button',
-      )}
-    >
-      <div className="d-flex gap-2 align-items-center">
-        <DAvatar name={contact.name} />
-        <div>
-          <p className="mb-0 fw-bold">{contact.name}</p>
-          <small className="text-gray-500">{contact.accountNumber}</small>
+    <div className="border-bottom border-gray-50 py-1">
+      <button
+        type="button"
+        onClick={handleSelectContact}
+        className={classNames(
+          'd-flex gap-2 align-items-center justify-content-between w-100 p-2',
+          'border-0 text-start rounded-2 bg-transparent hover:bg-gray-50 quick-action-button',
+        )}
+      >
+        <div className="d-flex gap-4 align-items-center">
+          <DIcon
+            className={`text-yellow-500 ${contact.isFavorite ? 'fill-yellow-500' : ''}`}
+            icon={contact.isFavorite ? 'Star' : 'Star'}
+          />
+          <div className="flex-1">
+            <p className="mb-0 fw-bold">{contact.name}</p>
+            <small className="text-gray-500">
+              {`${contact.bank} - ${contact.accountNumber}`}
+            </small>
+          </div>
         </div>
-      </div>
-      <DIcon
-        className={`text-yellow-500 ${contact.isFavorite ? 'fill-yellow-500' : ''}`}
-        icon={contact.isFavorite ? 'Star' : 'Star'}
-      />
-    </button>
+        <DIcon
+          className="ms-auto"
+          color="primary"
+          icon="ChevronRight"
+        />
+      </button>
+    </div>
   );
 }
